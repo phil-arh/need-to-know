58447ff67901048852331dc8d9b79d84e3eb7eca
Phil Howe <phil.a.r.howe@gmail.com>
2020-08-31 23:23:14 +0100

    Start adding docs to index.js

--- index.js
+++ index.js
@@ -9,28 +9,32 @@ const unlist = (list) => (R.length(list) === 1 ? list[0] : list);
 
 const applyToArgs = R.curry((args, fn) => fn(...args));
 
+/**
+ * @function
+ * @private
+ * @param {Array} paths An array of paths into a nested object
+ * @param {Object} obj A nested object with these paths
+ * Given an array of paths and a nested object, return an object filtered to
+ * contain only those paths
+ * @example
+ *  const obj = { a: 1, b: { c: 2, d: 3 }, e: 4 };
+ *  const paths = [['b', 'c'], ['e']]
+ *  pickPaths(paths, obj);
+ *  // => { b: { c: 2 }, e: 4 }
+ */
 const pickPaths = (() => {
-  const makeFromPath = function makeFromPath(keyArray, inputObject) {
-    const key = R.head(keyArray);
-    const restOfKeys = R.tail(keyArray);
-    return R.isEmpty(restOfKeys)
-      ? R.objOf(key, inputObject[key])
-      : R.objOf(key, makeFromPath(restOfKeys, inputObject[key]));
+  const makeFromPath = function makeFromPath(path, obj) {
+    return R.path(path, obj)
+      ? R.reduceRight((acc, key) => R.objOf(key, acc), R.path(path, obj), path)
+      : undefined;
   };
-
-  const strictMakeFromPath = function strictMakeFromPath(path, obj) {
-    return R.path(path, obj) === undefined
-      ? undefined
-      : makeFromPath(path, obj);
-  };
-
   // eslint-disable-next-line no-shadow
-  return function pickPaths(paths, map) {
+  return function pickPaths(paths, obj) {
     return R.reduce(
       (acc, keyPath) => {
-        const pathObject = strictMakeFromPath(keyPath, map);
+        const pathObject = makeFromPath(keyPath, obj);
         return pathObject
-          ? R.merge(acc, pathObject)
+          ? R.mergeDeepRight(acc, pathObject)
           : acc;
       },
       new Object(),
@@ -38,6 +42,22 @@ const pickPaths = (() => {
   };
 })();
 
+/**
+ * @function
+ * @public
+ * @param {Object} roleSchema The role schema for this setup
+ * @param {String} action The action type - enum create|write|update|delete
+ * @param {Array} userRoles The user's roles, e.g. ['accountant', 'manager']
+ * @param {String} dataType The data type of the input - e.g. 'users'
+ * Given an action, a data type, and the user's roles, check whether the user
+ * is ever allowed to perform the set action on the data type. This is not as
+ * specific as checking whether the user can operate on a particular document,
+ * but can be used as a preliminary check before operating on particular
+ * documents. In a large organisation there will be many combinations of role
+ * and data type in which actions will not be permitted at all - financially-
+ * sensitive data, for instance, will normally only be accessible to senior
+ * management and the finance department
+ */
 const userIsPermittedTo = R.curry(function userIsPermittedTo(
   roleSchema, action, userRoles, dataType,
 ) {
@@ -72,7 +92,7 @@ const filterDocuments = R.curry(function filterDocuments(
     )),
   ).map(
     (doc) => R.ifElse(
-      R.any((x) => x(doc, options) === true),
+      R.any((fn) => fn(doc, options) === true),
       R.always(doc),
       R.pipe(
         R.chain(R.tryCatch(
